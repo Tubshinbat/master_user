416be9564283d5d566049ecdd8766cc99a8fc19e
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-10-09 15:46:28 +0800

    a

--- src/axios-base.js
+++ src/axios-base.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:1003/api/v1/",
-  // baseURL: "https://admin.node.mn/api/",
+  // baseURL: "http://localhost:1003/api/v1/",
+  baseURL: "https://master.node.mn/api/",
 });
 
 instance.defaults.withCredentials = true;
